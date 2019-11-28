e5dd2c8dee66049237d09543c013e80e6d633adb
winewei <winewei@yeah.net>
2019-11-28 09:48:02 +0800

    update memcached

--- public/testMemcached.php
+++ public/testMemcached.php
@@ -1,5 +1,5 @@
 <?php
-    $memcache = new Memcached;
+    $memcache = new Memcached();
     $memcache -> addServer(memcache, 11211) or die ("Could not connect Memcached Server");
     $memcache -> set ('key','Connect Memcached Server sussucfull');
     $get = $memcache -> get('key');
